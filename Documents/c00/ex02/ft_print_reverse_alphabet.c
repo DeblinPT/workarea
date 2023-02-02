2255f6e9d151f046a0a9a8613ac266085db80a41
Andre Filipe Dimas Dos Santos <adimas-d@c1r19s1.42lisboa.com>
2023-02-02 18:08:33 +0000

    vfd

---
+++ Documents/c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adimas-d < adimas-d@student.42lisboa.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 18:00:56 by adimas-d          #+#    #+#             */
+/*   Updated: 2023/02/02 18:05:31 by adimas-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
+
+/*
+int		main()
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}*/
