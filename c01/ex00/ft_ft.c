9892959a0c46c5a800bf662da15b574ee499e02e
Andre Filipe Dimas Dos Santos <adimas-d@c1r19s1.42lisboa.com>
2023-02-04 18:26:49 +0000

    sdgv

--- c01/ex00/ft_ft.c
+++ c01/ex00/ft_ft.c
@@ -6,7 +6,7 @@
 /*   By: adimas-d < adimas-d@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 11:38:45 by adimas-d          #+#    #+#             */
-/*   Updated: 2023/02/04 17:50:43 by adimas-d         ###   ########.fr       */
+/*   Updated: 2023/02/04 18:21:09 by adimas-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@
 void	ft_ft(int *nbr)
 {
 	int	num;
+
 	num = 42;
 	*nbr = num;
-	
-	write(1, nbr, 2);
 }
 
-int	main(void)
+/*int	main(void)
 {
-	int*	nbr;
+	int	*nbr;
 
 	ft_ft(nbr);
+	printf("%d\n", *nbr);
 	return (0);
-}
+}*/
