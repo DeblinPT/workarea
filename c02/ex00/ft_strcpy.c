c0a53823fb697f84f9c7f829762a7bcb0eca225a
Andre Filipe Dimas Dos Santos <adimas-d@c1r19s1.42lisboa.com>
2023-02-06 04:08:34 +0000

    xcb

---
+++ c02/ex00/ft_strcpy.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adimas-d <adimas-d@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 01:00:56 by adimas-d          #+#    #+#             */
+/*   Updated: 2023/02/06 01:33:08 by adimas-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (0);
+}
+
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	char *src;
+	char *dest;
+	char *ret;
+
+	src = calloc(11, sizeof(char));
+	src = "0123456789";
+	dest = calloc(11, sizeof(char));
+	ret = ft_strcpy(dest, src);	
+	printf("src: %s\n", src);
+	printf("dest: %s\n", dest);
+	printf("ret: %s\n", ret);
+	return (0);
+}*/
