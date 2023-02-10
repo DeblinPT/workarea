0db705e1ae4b7ff98cd5482761b7285e9944f43e
Andre Filipe Dimas Dos Santos <adimas-d@c1r19s1.42lisboa.com>
2023-02-10 20:52:31 +0000

    sg

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,13 +6,15 @@
 /*   By: adimas-d <adimas-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 20:35:12 by adimas-d          #+#    #+#             */
-/*   Updated: 2023/02/09 13:01:27 by adimas-d         ###   ########.fr       */
+/*   Updated: 2023/02/10 20:44:59 by adimas-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
+	unsigned char	s1x;
+	unsigned char	s2x;
 
 	i = 0;
 	if (n == 0)
@@ -21,7 +23,9 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	{
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	s1x = s1[i];
+	s2x = s2[i];
+	return (s1x - s2x);
 }
 
 #include <stdio.h>
