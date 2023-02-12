b0f3acd474d5ef3fad07b20b56731694ca490cd5
Andre Filipe Dimas Dos Santos <adimas-d@c1r19s1.42lisboa.com>
2023-02-12 00:24:52 +0000

    dgbhfngh

--- c03/ex05/test.c
+++ c03/ex05/test.c
@@ -1,67 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: adimas-d <adimas-d@student.42lisboa.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/09 22:32:33 by adimas-d          #+#    #+#             */
-/*   Updated: 2023/02/12 00:24:04 by adimas-d         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
+	int				src_size;
 	unsigned int	dest_size;
-	unsigned int	src_size;
-	int				i;
-
-	dest_size = 0;
-	src_size = 0;
+	unsigned int	i;
 	i = 0;
-	while (dest[dest_size] != 0)
-		dest_size++;
-	while (src[src_size] != 0)
+	src_size = 0;
+	dest_size = 0;
+	while (src[src_size])
 		src_size++;
+
+	if (size == 0)
+		return (src_size);
+
+	while (dest[dest_size])
+		dest_size++;
+
 	if (size <= dest_size)
 		return (size + src_size);
+
+	i = 0;
 	while (size > (dest_size + i + 1) && src[i])
 	{
 		dest[dest_size + i] = src[i];
 		i++;
 	}
+
 	dest[dest_size + i] = '\0';
 	return (src_size + dest_size);
-}
-
-/*
-Usar "gcc -Wall -Wextra -Werror -g ft_strlcat.c -lbsd" no shell para correr
-
-unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
-{
-	unsigned int	d_idx;
-	unsigned int	s_idx;
-	unsigned int	res;
-
-	d_idx = 0;
-	s_idx = 0;
-	res = 0;
-	while (dest[d_idx] != '\0')
-		d_idx++;
-	while (src[res] != '\0')
-		res++;
-	if (size <= d_idx)
-		res += size;
-	else
-		res += d_idx;
-	while (src[s_idx] != '\0' && (d_idx + 1) < size)
-	{
-		dest[d_idx] = src[s_idx];
-		d_idx++;
-		s_idx++;
-	}
-	dest[d_idx] = '\0';
-	return (res);
 }
 
 #include <stdio.h>
@@ -106,4 +71,3 @@ int	main(void)
 	ft_strlcat_test(dest, ft_dest, src, 10);
 	return (0);
 }
-*/
