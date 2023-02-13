43606ccaff309da9be8e3c1f1f854f7bd177d78f
Andre Filipe Dimas Dos Santos <adimas-d@c1r19s1.42lisboa.com>
2023-02-13 23:06:58 +0000

    dfhgd

---
+++ Documents/c00/ex08/ft_print_combn.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adimas-d < adimas-d@student.42lisboa.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 22:08:09 by adimas-d          #+#    #+#             */
+/*   Updated: 2023/02/04 15:28:50 by adimas-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_print_combn(int n)
+{
+	if (n > 1 || n < 9)
+	{
+		return ;
+	}
+}
+
+void	ft_print(int algarismo)
+{
+	write(1, &algarismo, 1);
+}
+
+int	main(void)
+{
+	ft_print_combn(2);
+}
