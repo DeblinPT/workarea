c011594ffcb5232e3cdb989ddb405e094d166028
Andre Filipe Dimas Dos Santos <adimas-d@c1r19s1.42lisboa.com>
2023-09-07 20:27:10 +0100

    sgsd

--- c00/ex00/ft_putchar.c
+++ c00/ex00/ft_putchar.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adimas-d < adimas-d@student.42lisboa.co    +#+  +:+       +#+        */
+/*   By: adimas-d <adimas-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 15:29:58 by adimas-d          #+#    #+#             */
-/*   Updated: 2023/02/02 18:05:33 by adimas-d         ###   ########.fr       */
+/*   Updated: 2023/05/10 16:00:08 by adimas-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	ft_putchar(char c)
 	write (1, &c, 1);
 }
 
-/*
+
 int	main(void)
 {
 	ft_putchar('d');
-}*/
+}
